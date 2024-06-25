aff5987b6768e2af98f2f6163a6f4a45ab714ed1
Paul Hagstrom <hagstrom@bu.edu>
2024-06-25 15:43:14 -0400

    Work around incorrect url returned by url method

--- commands/nudge.js
+++ commands/nudge.js
@@ -7,14 +7,14 @@ module.exports = {
 	}
 	const targetChannel = message.mentions.channels.first();
 	message.channel.send('.').then(placed => {
-	    const jumpsource = `Corresponding moment in ${placed.url}`;
+	    const jumpsource = `Corresponding moment in ${placed.url.replace("discordapp.com", "discord.com")}`;
 	    const targetmsg = "Conversation nudged here from another channel.";
 	    const targetEmbed = {
 		description: `${targetmsg}\n${jumpsource}`,
 	    };
 	    //targetChannel.send({ embeds: [targetEmbed] }).then(sent => {
 	    targetChannel.send(`${targetmsg}\n${jumpsource}`).then(sent => {
-		const jumptarget = `Corresponding moment in ${sent.url}`;
+		const jumptarget = `Corresponding moment in ${sent.url.replace("discordapp.com", "discord.com")}`;
 		const sourcemsg = "Conversation seems like it might fit better in another channel.";
 		const sourceEmbed = {
 		    description: `${sourcemsg}\n${jumptarget}`,
