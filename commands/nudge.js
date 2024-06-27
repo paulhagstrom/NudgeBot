0c712e0898bc1b7a61a69254e723204590d3ee16
Paul Hagstrom <hagstrom@bu.edu>
2024-06-27 14:37:14 +0000

    Local editing until it worked

--- commands/nudge.js
+++ commands/nudge.js
@@ -6,21 +6,24 @@ module.exports = {
 	    return message.reply('You need to mention what channel you want to nudge to.');
 	}
 	const targetChannel = message.mentions.channels.first();
-	message.channel.send('.').then(placed => {
+	const emptyEmbed = {
+	    description: "Placeholder for jump link.",
+	    };
+	message.channel.send({embed: emptyEmbed}).then(placed => {
 	    const jumpsource = `Corresponding moment in ${placed.url.replace("discordapp.com", "discord.com")}`;
 	    const targetmsg = "Conversation nudged here from another channel.";
 	    const targetEmbed = {
 		description: `${targetmsg}\n${jumpsource}`,
 	    };
-	    //targetChannel.send({ embeds: [targetEmbed] }).then(sent => {
-	    targetChannel.send(`${targetmsg}\n${jumpsource}`).then(sent => {
+	    targetChannel.send({ embed: targetEmbed }).then(sent => {
+	    //targetChannel.send(`${targetmsg}\n${jumpsource}`).then(sent => {
 		const jumptarget = `Corresponding moment in ${sent.url.replace("discordapp.com", "discord.com")}`;
 		const sourcemsg = "Conversation seems like it might fit better in another channel.";
 		const sourceEmbed = {
 		    description: `${sourcemsg}\n${jumptarget}`,
 		};
-		// placed.edit({ embeds: [sourceEmbed] });
-		placed.edit(`${sourcemsg}\n${jumptarget}`);
+		placed.edit({ embed: sourceEmbed });
+		//placed.edit(`${sourcemsg}\n${jumptarget}`);
 	    });
 	});
     },
